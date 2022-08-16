028f3594b5598b1fc3003f5cc2be38ae5c7b0e0c
Jose Antonio <83122668+jmatute-1511@users.noreply.github.com>
2022-08-16 15:45:21 +0200

    create whitenova function

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const moment = require("moment");
 const readlinesync = require("readline-sync")
 require("dotenv").config()
 
+const sleep = (ms) =>{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+const req_to_api = async (string, access_token) =>{
+    let res = await axios.get(`https://api.intra.42.fr/v2/${string}`,{
+        headers:{
+            "Authorization" : `Bearer ${access_token}`
+        }
+    })
+    return(res.data)
+}
 const get_token = async () =>{
 
     let res = await axios.post("https://api.intra.42.fr/oauth/token", {
@@ -24,13 +35,10 @@ const get_id = async (access_token, login)=>{
         return(res.data[0]["id"])
     return(null)
 }
-
-const get_hours = async (access_token, id) =>{
-
+const get_now_whitenova = ()=>{
     let first_time = moment("2022-07-29")
     let relative_time = moment(first_time).add(14, 'days')
     let time_now =moment()
-    let timeofnova = [0, 0, 0] 
     if (time_now > relative_time)
     {
         while (time_now > relative_time)
@@ -40,25 +48,26 @@ const get_hours = async (access_token, id) =>{
         }
     }
     let cut = first_time.format().slice(0,first_time.format().indexOf("T"));
+    return(cut)
+
+}
+
+const get_hours = async (access_token, id, now_whitenova) =>{
+
+    let timeofnova = [0, 0, 0];
+
     if (id != null){
-        let res = await axios.get(`https://api.intra.42.fr/v2/users/${id}/locations_stats?begin_at=${cut}`,{
-            headers:{
-                "Authorization" : `Bearer ${access_token}`
-            }
-         })  
-        sum_hours(res.data, timeofnova)
-        console.log(`Horas de log : ${timeofnova[0]} days ${timeofnova[1]}:${timeofnova[2]}`)
+        await sleep(550)
+        let res = await req_to_api(`/users/${id}/locations_stats?begin_at=${now_whitenova}`, access_token)
+        sum_hours(res, timeofnova)
+        console.log(`Hours being logged in : ${timeofnova[0]} days and ${timeofnova[1]}:${timeofnova[2]} hours `)
     }
     else{
         console.log("Invalid login!")
     }
 }
-const get_events = async (intra_token, id) =>{
-    let res = await axios.get()
-
-}
 const sum_hours = (my_hours, timeofnova) =>{
-
+    
     let it = 0;
     let hours_split= JSON.stringify(my_hours).split(",")
     while(it < hours_split.length)
@@ -78,18 +87,47 @@ const sum_hours = (my_hours, timeofnova) =>{
     }
     return(timeofnova)
 }
-const white_nova = async (intra_token, login)=>{
 
-    let id = await get_id(intra_token, login);
-    await get_hours(intra_token, id);
+const get_events = async (access_token, id, now_whitenova) =>{
     
+    let time_now = moment().format()
+    let cut = time_now.slice(0,time_now.indexOf("T"))
+    
+    await sleep(550)
+    let res = await req_to_api(`users/${id}/events_users?range[updated_at]=${now_whitenova},${cut}`,access_token)
+    if (res.length == 0)
+        console.log("You have not attended events")
+    else{
+        console.log(`Yo have attended ${res.length} events`)
+    }
+}
+const get_corrections = async (access_token,id, now_whitenova) =>{
+    
+    let time_now = moment().format()
+    let cut = time_now.slice(0, time_now.indexOf("T"))
+    await sleep(550)
+    let res = await req_to_api(`/users/${id}/scale_teams/as_corrector?range[created_at]=${now_whitenova},${cut}`, access_token)
+    if (res.length == 0){
+        console.log(`You have not made corrections in this cycle`)
+    }
+    else{
+        console.log(`ou have made ${res.length} corrections`)
+    }
+
+}
+const white_nova = async (intra_token, id)=>{
+    let now_whitenova = get_now_whitenova();
+    await get_hours(intra_token, id, now_whitenova)
+    await get_events(intra_token, id, now_whitenova)
+    await get_corrections(intra_token,id, now_whitenova)
 }
 
 const select_action = async (intra_token, question) =>{
 
     let compare = question.split(" ")
+    let id = await get_id(intra_token, compare[1]);
     if (compare[0] == 'whitenova')
-        await white_nova(intra_token, compare[1])
+        await white_nova(intra_token, id)
 }
 
 const main = async() =>
